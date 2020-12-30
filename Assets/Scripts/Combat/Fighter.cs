6391be88b04076c87a11add7c3067c95f8e51ad3
RafaF196 <RafaF196@gmail.com>
2020-12-30 12:24:10 +0100

    Video 38

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -8,6 +8,7 @@ namespace RPG.Combat
     {
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
 
         Transform target;
         float timeSinceLastAttack = 0;
@@ -35,7 +36,14 @@ namespace RPG.Combat
             {
                 GetComponent<Animator>().SetTrigger("attack");
                 timeSinceLastAttack = 0;
+                // This will trigger Hit event (animation)
             }
+        }
+
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
@@ -54,6 +62,5 @@ namespace RPG.Combat
             target = null;
         }
 
-        void Hit() { }
     }
 }
