647a92a3a5c4df920bb77a6abb9822881fdca71f
RafaF196 <RafaF196@gmail.com>
2020-12-31 15:21:54 +0100

    Video 46

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 10f;
+
+        private void Update()
+        {
+            if (DistanceToPlayer() < chaseDistance)
+            {
+                // Chase
+            }
+        }
+
+        private float DistanceToPlayer()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            Vector3.Distance(player.transform.position, transform.position);
+        }
+    }
+}
