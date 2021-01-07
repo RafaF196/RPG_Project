1397f57e63290113ed448f12a22dbcd811f289fd
RafaF196 <RafaF196@gmail.com>
2021-01-07 12:12:27 +0100

    Video 113

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -8,12 +8,25 @@ namespace RPG.Combat
     public class Weapon : ScriptableObject
     {
         [SerializeField] AnimatorOverrideController animatorOverride = null;
-        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] GameObject equipedPrefab = null;
+
+        [SerializeField] float weaponDamage = 5f;
+        [SerializeField] float weaponRange = 2f;
 
         public void Spawn(Transform handTransform, Animator animator)
         {
-            Instantiate(weaponPrefab, handTransform);
-            animator.runtimeAnimatorController = animatorOverride;
+            if (equipedPrefab != null) Instantiate(equipedPrefab, handTransform);
+            if (animatorOverride != null) animator.runtimeAnimatorController = animatorOverride;
+        }
+
+        public float getWeaponDamage()
+        {
+            return weaponDamage;
+        }
+
+        public float getWeaponRange()
+        {
+            return weaponRange;
         }
 
     }
