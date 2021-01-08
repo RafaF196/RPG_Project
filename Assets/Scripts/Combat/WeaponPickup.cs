88bf2347bfb0d0f2322443c8c9ddd8760eff2903
RafaF196 <RafaF196@gmail.com>
2021-01-08 13:02:23 +0100

    Video 128

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float respawnTime = 2f;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -14,6 +16,32 @@ namespace RPG.Combat
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
                 Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
+            }
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            HidePickup();
+            yield return new WaitForSeconds(seconds);
+            ShowPickup();
+        }
+
+        private void HidePickup()
+        {
+            GetComponent<Collider>().enabled = false;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        private void ShowPickup()
+        {
+            GetComponent<Collider>().enabled = true;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(true);
             }
         }
     }
