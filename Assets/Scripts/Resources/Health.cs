38b9fb5cb174dc7144f135b0af2759617b0e11e3
RafaF196 <RafaF196@gmail.com>
2021-01-09 01:03:06 +0100

    Video 136

--- Assets/Scripts/Resources/Health.cs
+++ Assets/Scripts/Resources/Health.cs
@@ -16,7 +16,7 @@ namespace RPG.Resources
 
         public void Start()
         {
-            healthPoints = GetComponent<BaseStats>().GetHealth();
+            healthPoints = GetComponent<BaseStats>().GetStat(Stat.Health);
         }
 
         public bool IsDead()
@@ -36,7 +36,7 @@ namespace RPG.Resources
 
         public float GetPercentage()
         {
-            return 100 * healthPoints / GetComponent<BaseStats>().GetHealth();
+            return 100 * healthPoints / GetComponent<BaseStats>().GetStat(Stat.Health);
         }
 
         private void Die()
@@ -53,7 +53,7 @@ namespace RPG.Resources
             Experience experience = instigator.GetComponent<Experience>();
             if (experience == null) return;
 
-            experience.GainExperience(GetComponent<BaseStats>().getExperienceReward());
+            experience.GainExperience(GetComponent<BaseStats>().GetStat(Stat.ExperienceReward));
         }
 
         public object CaptureState()
