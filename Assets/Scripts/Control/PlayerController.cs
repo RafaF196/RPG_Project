7e32f6c339fc4d8ac061bd4e76c5f4d61e44cf1a
RafaF196 <RafaF196@gmail.com>
2021-01-10 23:45:14 +0100

    Video 162

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -22,6 +22,7 @@ namespace RPG.Control
 
         [SerializeField] CursorMapping[] cursorMappings = null;
         [SerializeField] float maxNavMeshProjectionDistance = 1f;
+        [SerializeField] float maxNavPathLength = 50f;
 
         private void Awake()
         {
@@ -117,7 +118,25 @@ namespace RPG.Control
 
             target = navMeshHit.position;
 
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+            if (!hasPath) return false;
+            if (path.status != NavMeshPathStatus.PathComplete) return false;
+            if (GetPathLength(path) > maxNavPathLength) return false;
+
             return true;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float total = 0;
+            if (path.corners.Length < 2) return total;
+            for (int i = 0; i < path.corners.Length-1; i++)
+            {
+                float distance = Vector3.Distance(path.corners[i], path.corners[i+1]);
+                total += distance;
+            }
+            return total;
         }
 
         private void SetCursor(CursorType type)
